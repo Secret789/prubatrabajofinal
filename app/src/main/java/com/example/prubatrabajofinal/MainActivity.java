f52c82b0abc3b1bdbfba675b7c53175eb84a0390
leowi <jhoshep_1999@hotmail.com>
2020-11-11 22:58:37 -0500

    Se agrego NinePatch v3

--- app/src/main/java/com/example/prubatrabajofinal/MainActivity.java
+++ app/src/main/java/com/example/prubatrabajofinal/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.prubatrabajofinal.View.Reproductor.Reproductor;
 
@@ -15,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
