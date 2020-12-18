91a5be3bc4b09166a31fc8823a65f92182633af0
almighty7 <38875101+almighty7@users.noreply.github.com>
2020-12-18 12:36:15 -0500

    chronometro

--- app/src/main/java/com/example/prubatrabajofinal/MainActivity.java
+++ app/src/main/java/com/example/prubatrabajofinal/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.prubatrabajofinal;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.prubatrabajofinal.View.Autenticacion.Ingresar;
 import com.example.prubatrabajofinal.View.Entrenamiento.Entrenamiento;
@@ -17,7 +21,10 @@ import com.example.prubatrabajofinal.View.Usuario.Usuario;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_start;
+    Chronometer chronometro;
+    Boolean correr=false;
+    long detenerse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,9 +47,26 @@ public class MainActivity extends AppCompatActivity {
                     new Entrenamiento()).commit();
         }
 
+        btn_start=findViewById(R.id.btn_start);
+        chronometro=findViewById(R.id.chronometro);
 
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChronometro();
+            }
+        });
 
     }
+
+    private void startChronometro() {
+        if(!correr){
+            chronometro.setBase(SystemClock.elapsedRealtime() - detenerse);
+            chronometro.start();
+            correr=true;
+        }
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener=
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
